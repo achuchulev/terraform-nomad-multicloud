38fa7150ca26002db64fb3029adca9dadbec08a8
Atanas Chuchulev <achuchulev@Atanass-MacBook-Pro.local>
2019-09-18 16:03:58 +0300

    update kitchen test

--- test/integration/default/state_check.rb
+++ test/integration/default/state_check.rb
@@ -1,60 +1,70 @@
 describe command('terraform output') do
-  its('stdout') { should include "aws-region1-nomad_server_private_ip" }
+  its('stdout') { should include "aws_vpc_id" }
   its('stderr') { should include '' }
   its('exit_status') { should eq 0 }
 end
 describe command('terraform output') do
-  its('stdout') { should include "aws-region1-nomad_server_tags" }
+  its('stdout') { should include "aws_vpc_name" }
   its('stderr') { should include '' }
   its('exit_status') { should eq 0 }
 end
 describe command('terraform output') do
-  its('stdout') { should include "aws-region1-nomad_client_private_ip" }
+  its('stdout') { should include "aws_subnet_ids" }
   its('stderr') { should include '' }
   its('exit_status') { should eq 0 }
 end
 describe command('terraform output') do
-  its('stdout') { should include "aws-region1-nomad_server_private_ips" }
+  its('stdout') { should include "aws_server_private_ips" }
   its('stderr') { should include '' }
   its('exit_status') { should eq 0 }
 end
 describe command('terraform output') do
-  its('stdout') { should include "aws-region1-nomad_client_tags" }
+  its('stdout') { should include "aws_client_private_ips" }
   its('stderr') { should include '' }
   its('exit_status') { should eq 0 }
 end
 describe command('terraform output') do
-  its('stdout') { should include "aws-region2-nomad_server_private_ip" }
+  its('stdout') { should include "aws_frontend_public_ip" }
   its('stderr') { should include '' }
   its('exit_status') { should eq 0 }
 end
 describe command('terraform output') do
-  its('stdout') { should include "aws-region2-nomad_server_tags" }
+  its('stdout') { should include "AWS_Nomad_UI_URL" }
   its('stderr') { should include '' }
   its('exit_status') { should eq 0 }
 end
 describe command('terraform output') do
-  its('stdout') { should include "aws-region2-nomad_client_private_ip" }
+  its('stdout') { should include "gcp_vpc_network_id" }
   its('stderr') { should include '' }
   its('exit_status') { should eq 0 }
 end
 describe command('terraform output') do
-  its('stdout') { should include "aws-region2-nomad_client_tags" }
+  its('stdout') { should include "gcp_vpc_network_subnet_id" }
   its('stderr') { should include '' }
   its('exit_status') { should eq 0 }
 end
 describe command('terraform output') do
-  its('stdout') { should include "frontend_server_public_ip" }
+  its('stdout') { should include "gcp_server_private_ips" }
   its('stderr') { should include '' }
   its('exit_status') { should eq 0 }
 end
 describe command('terraform output') do
-  its('stdout') { should include "gcp-region-nomad_server_private_ip" }
+  its('stdout') { should include "gcp_client_private_ips" }
   its('stderr') { should include '' }
   its('exit_status') { should eq 0 }
 end
 describe command('terraform output') do
-  its('stdout') { should include "gcp-region-nomad_client_private_ip" }
+  its('stdout') { should include "gcp_frontend_public_ip" }
+  its('stderr') { should include '' }
+  its('exit_status') { should eq 0 }
+end
+describe command('terraform output') do
+  its('stdout') { should include "gcp_Nomad_UI_URL" }
+  its('stderr') { should include '' }
+  its('exit_status') { should eq 0 }
+end
+describe command('terraform output') do
+  its('stdout') { should include "client_vpn_id" }
   its('stderr') { should include '' }
   its('exit_status') { should eq 0 }
 end
